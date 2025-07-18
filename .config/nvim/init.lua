1bf779b9ff05e0297079e3fb4671b53ff339a756
stekra <stekra@me.com>
2025-07-18 12:10:00 +0200

    simplify nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,439 +1,102 @@
+-- vim.opt.number = true
+-- vim.opt.relativenumber = true
+vim.opt.wrap = true
+vim.opt.scrolloff = 10
+vim.opt.sidescrolloff = 8
+vim.opt.showbreak = "↪"
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "no"
+vim.opt.showmatch = true
+vim.opt.matchtime = 2
+vim.opt.cmdheight = 1
+-- vim.opt.completeopt = "menuone,noinsert,noselect"
+-- vim.opt.showmode = false
+vim.opt.pumheight = 10
+-- vim.opt.pumblend = 10
+-- vim.opt.winblend = 0
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.undofile = true
+vim.opt.updatetime = 250
+vim.opt.autoread = true
+
+-- vim.opt.hidden = true
+vim.opt.errorbells = false
+vim.opt.backspace = "indent,eol,start"
+-- vim.opt.autochdir = false
+vim.opt.iskeyword:append("-")
+vim.opt.path:append("**")
+vim.opt.mouse = "a"
+vim.opt.clipboard:append("unnamedplus")
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- vim.opt.relativenumber = true
-vim.opt.signcolumn = "no"
-vim.opt.mouse = "a"
--- vim.opt.showmode = false
-vim.opt.clipboard = "unnamedplus"
-vim.opt.scrolloff = 5
-vim.opt.undofile = true
-vim.opt.showbreak = "↪"
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.updatetime = 250
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-vim.opt.inccommand = "split"
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
--- vim.opt.cmdheight = 0
-vim.opt.termguicolors = true
+vim.keymap.set({ "n", "v" }, "j", "gj", { desc = "Cursor down (line wrap)" })
+vim.keymap.set({ "n", "v" }, "k", "gk", { desc = "Cursor up (line wrap)" })
+vim.keymap.set({ "n", "v" }, "n", "nzzv", { desc = "Next search result (centered)" })
+vim.keymap.set({ "n", "v" }, "N", "<Nzzv", { desc = "Previous search result (centered)" })
+vim.keymap.set({ "n", "v" }, "<C-u>", "<C-u>zz", { desc = "Half page up (centered)" })
+vim.keymap.set({ "n", "v" }, "<C-d>", "<C-d>zz", { desc = "Half page down (centered)" })
 
--- transparent background
--- (think this should be run after theme is set but idk)
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
-vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left split" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right split" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to bottom split" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to top split" })
 
-vim.api.nvim_create_autocmd("User", {
-	pattern = "VeryLazy",
-	callback = function()
-		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-		vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
-		vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
-		vim.api.nvim_set_hl(0, "Terminal", { bg = "none" })
-		vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
-		vim.api.nvim_set_hl(0, "FoldColumn", { bg = "none" })
-		vim.api.nvim_set_hl(0, "Folded", { bg = "none" })
-		vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
-	end,
-})
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
 
-vim.keymap.set({ "n", "v" }, "j", "gj")
-vim.keymap.set({ "n", "v" }, "k", "gk")
-vim.keymap.set({ "n", "v" }, "<C-u>", "<C-u>zz")
-vim.keymap.set({ "n", "v" }, "<C-d>", "<C-d>zz")
+vim.keymap.set("v", "<", "<gv", { desc = "Indent left and reselect" })
+vim.keymap.set("v", ">", ">gv", { desc = "Indent right and reselect" })
+
+vim.keymap.set("n", "<leader>e", ":Explore<CR>", { desc = "Open file explorer" })
+vim.keymap.set("n", "<leader>ff", ":find ", { desc = "Find file" })
 
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", {})
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", {})
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", {})
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", {})
+
+vim.keymap.set("n", "<leader>rc", ":e ~/.config/nvim/init.lua<CR>", { desc = "Edit config" })
+
+local augroup = vim.api.nvim_create_augroup("UserConfig", {})
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+  desc = "Highlight on yank",
+  group = augroup,
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		error("Error cloning lazy.nvim:\n" .. out)
-	end
-end
-
-vim.opt.rtp:prepend(lazypath)
-
--- Only show in-line diagnostics of current line
-vim.diagnostic.config({ virtual_text = false })
-
-require("lazy").setup({
-	"tpope/vim-sleuth", -- Auto tabstop and shiftwidth
-
-	{ -- Git signs
-		"lewis6991/gitsigns.nvim",
-		opts = {
-			signs = {
-				add = { text = "+" },
-				change = { text = "~" },
-				delete = { text = "_" },
-				topdelete = { text = "‾" },
-				changedelete = { text = "~" },
-			},
-		},
-	},
-
-	{ -- Fuzzy Finder (files, lsp, etc)
-		"nvim-telescope/telescope.nvim",
-		event = "VimEnter",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			{
-				"nvim-telescope/telescope-fzf-native.nvim",
-				build = "make",
-				cond = function()
-					return vim.fn.executable("make") == 1
-				end,
-			},
-			{ "nvim-telescope/telescope-ui-select.nvim" },
-		},
-		config = function()
-			require("telescope").setup({
-				pickers = {
-					find_files = { theme = "dropdown" },
-					live_grep = { theme = "dropdown" },
-					oldfiles = { theme = "dropdown" },
-					buffers = { theme = "dropdown" },
-				},
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown(),
-					},
-				},
-			})
-
-			pcall(require("telescope").load_extension, "fzf")
-			pcall(require("telescope").load_extension, "ui-select")
-
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>h", builtin.help_tags, {})
-			vim.keymap.set("n", "<leader>f", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>g", builtin.live_grep, {})
-			vim.keymap.set("n", "<leader>o", builtin.oldfiles, {})
-			vim.keymap.set("n", "<leader><leader>", builtin.buffers, {})
-
-			vim.keymap.set("n", "<leader>/", function()
-				builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-					previewer = false,
-				}))
-			end)
-
-			vim.keymap.set("n", "<leader>s/", function()
-				builtin.live_grep({
-					grep_open_files = true,
-					prompt_title = "Live Grep in Open Files",
-				})
-			end)
-		end,
-	},
-
-	{ -- Lua for nvim
-		"folke/lazydev.nvim",
-		ft = "lua",
-		opts = {
-			library = {
-				{ path = "luvit-meta/library", words = { "vim%.uv" } },
-			},
-		},
-	},
-	{ "Bilal2453/luvit-meta", lazy = true },
-
-	{ -- LSP
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			{ "williamboman/mason.nvim", config = true },
-			"williamboman/mason-lspconfig.nvim",
-			"WhoIsSethDaniel/mason-tool-installer.nvim",
-			{
-				"j-hui/fidget.nvim",
-				opts = {
-					progress = {
-						ignore = {
-							function(msg)
-								return msg.lsp_client.name == "pylsp" and string.find(msg.title, "lint:")
-							end,
-						},
-					},
-				},
-			},
-			"hrsh7th/cmp-nvim-lsp",
-		},
-		config = function()
-			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
-				callback = function()
-					vim.keymap.set("n", "gd", vim.lsp.buf.definition)
-					vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references)
-					vim.keymap.set("n", "<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols)
-					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
-				end,
-			})
-
-			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
-
-			local servers = {
-				-- clangd = {},
-				-- gopls = {},
-				-- pyright = {},
-				rust_analyzer = {},
-				-- ts_ls = {},
-				lua_ls = {
-					settings = {
-						Lua = {
-							completion = {
-								callSnippet = "Replace",
-							},
-							diagnostics = { disable = { "missing-fields" } },
-						},
-					},
-				},
-			}
-			require("mason").setup()
-			local ensure_installed = vim.tbl_keys(servers or {})
-			vim.list_extend(ensure_installed, {
-				"stylua",
-				"rust_analyzer",
-			})
-			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
-
-			require("mason-lspconfig").setup({
-				handlers = {
-					function(server_name)
-						local server = servers[server_name] or {}
-						server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-						require("lspconfig")[server_name].setup(server)
-					end,
-				},
-			})
-		end,
-	},
-
-	{ -- Autoformat
-		"stevearc/conform.nvim",
-		event = { "BufWritePre" },
-		cmd = { "ConformInfo" },
-		keys = {
-			{
-				"<leader>f",
-				function()
-					require("conform").format({ async = true, lsp_format = "fallback" })
-				end,
-				mode = "",
-				desc = "[F]ormat buffer",
-			},
-		},
-		opts = {
-			notify_on_error = false,
-			format_on_save = function(bufnr)
-				local disable_filetypes = { c = true, cpp = true }
-				local lsp_format_opt
-				if disable_filetypes[vim.bo[bufnr].filetype] then
-					lsp_format_opt = "never"
-				else
-					lsp_format_opt = "fallback"
-				end
-				return {
-					timeout_ms = 500,
-					lsp_format = lsp_format_opt,
-				}
-			end,
-			formatters_by_ft = {
-				lua = { "stylua" },
-			},
-		},
-	},
-
-	{ -- Autocompletion
-		"hrsh7th/nvim-cmp",
-		event = "InsertEnter",
-		dependencies = {
-			{
-				"L3MON4D3/LuaSnip",
-				build = (function()
-					if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
-						return
-					end
-					return "make install_jsregexp"
-				end)(),
-			},
-			"saadparwaiz1/cmp_luasnip",
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-path",
-		},
-		config = function()
-			local cmp = require("cmp")
-			local luasnip = require("luasnip")
-			luasnip.config.setup({})
-
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						luasnip.lsp_expand(args.body)
-					end,
-				},
-				completion = { completeopt = "menu,menuone,noinsert" },
-				mapping = cmp.mapping.preset.insert({
-					["<C-n>"] = cmp.mapping.select_next_item(),
-					["<C-p>"] = cmp.mapping.select_prev_item(),
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-
-					-- ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-					-- ['<Tab>'] = cmp.mapping.confirm({ select = true }),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-					["<C-Space>"] = cmp.mapping.complete({}),
-				}),
-				sources = {
-					{
-						name = "lazydev",
-						group_index = 0,
-					},
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-					{ name = "path" },
-				},
-			})
-		end,
-	},
-
-	{ -- In-line diagnostic style
-		"rachartier/tiny-inline-diagnostic.nvim",
-		event = "VeryLazy", -- Or `LspAttach`
-		priority = 1000, -- needs to be loaded in first
-		config = function()
-			require("tiny-inline-diagnostic").setup({
-				preset = "minimal",
-			})
-		end,
-	},
-
-	-- Themes
-
-	{
-		"zenbones-theme/zenbones.nvim",
-		dependencies = "rktjmp/lush.nvim",
-	},
-	{
-		"datsfilipe/vesper.nvim",
-		opts = {
-			transparent = true,
-			italics = {
-				comments = true,
-				keywords = false,
-				functions = false,
-				strings = false,
-				variables = false,
-			},
-		},
-		config = function(_, opts)
-			require("vesper").setup(opts)
-			-- vim.cmd.colorscheme("vesper")
-		end,
-	},
-	{
-		"kvrohit/rasmus.nvim",
-		config = function()
-			-- vim.g.rasmus_variant = "monochrome"
-			vim.g.rasmus_transparent = true
-			-- vim.cmd.colorscheme("rasmus")
-		end,
-	},
-
-	-- Plugins
-
-	{ -- Highlight todo, notes, etc in comments
-		"folke/todo-comments.nvim",
-		event = "VimEnter",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = { signs = false },
-	},
-
-	{ -- Collection of various small independent plugins/modules
-		"echasnovski/mini.nvim",
-		config = function()
-			require("mini.ai").setup({ n_lines = 500 })
-		end,
-	},
-
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			require("lualine").setup({
-				options = {
-					icons_enabled = false,
-					component_separators = "|",
-					section_separators = "",
-				},
-			})
-		end,
-	},
-
-	{ -- Highlight, edit, and navigate code
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		main = "nvim-treesitter.configs",
-		opts = {
-			ensure_installed = {
-				"bash",
-				"c",
-				"diff",
-				"html",
-				"lua",
-				"luadoc",
-				"markdown",
-				"markdown_inline",
-				"query",
-				"vim",
-				"vimdoc",
-			},
-			auto_install = true,
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = { "ruby" },
-			},
-			indent = { enable = true, disable = { "ruby" } },
-		},
-	},
-}, {
-	ui = {
-		icons = vim.g.have_nerd_font and {} or {
-			cmd = "⌘",
-			config = "🛠",
-			event = "📅",
-			ft = "📂",
-			init = "⚙",
-			keys = "🗝",
-			plugin = "🔌",
-			runtime = "💻",
-			require = "🌙",
-			source = "📄",
-			start = "🚀",
-			task = "📌",
-			lazy = "💤 ",
-		},
-	},
+vim.api.nvim_create_autocmd("VimResized", {
+  desc = "Auto-resize splits on window resize",
+  group = augroup,
+  callback = function()
+    vim.cmd("tabdo wincmd =")
+  end,
 })
 
--- vim: ts=2 sts=2 sw=2 et
+vim.api.nvim_create_autocmd("Colorscheme", {
+  desc = "Set theme background to none",
+  group = augroup,
+  callback = function()
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+    vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+    end,
+})
+vim.cmd.colorscheme("default")
+
