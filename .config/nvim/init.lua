e5e9e630968a2572d6e3b0b21fb74eee3845fc92
stekra <stekra@me.com>
2025-08-21 00:02:27 +0200

    nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,102 +1,99 @@
--- vim.opt.number = true
--- vim.opt.relativenumber = true
-vim.opt.wrap = true
+-- vim.opt.signcolumn = "yes:1"
+vim.opt.signcolumn = "no"
+vim.opt.showbreak = "↪"
 vim.opt.scrolloff = 10
 vim.opt.sidescrolloff = 8
-vim.opt.showbreak = "↪"
-
+vim.opt.mousescroll = "ver:1,hor:1"
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
-vim.opt.autoindent = true
 
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.incsearch = true
 
-vim.opt.termguicolors = true
-vim.opt.signcolumn = "no"
 vim.opt.showmatch = true
-vim.opt.matchtime = 2
-vim.opt.cmdheight = 1
--- vim.opt.completeopt = "menuone,noinsert,noselect"
--- vim.opt.showmode = false
-vim.opt.pumheight = 10
--- vim.opt.pumblend = 10
--- vim.opt.winblend = 0
+
 vim.opt.splitbelow = true
 vim.opt.splitright = true
+vim.opt.winborder = "single"
 
 vim.opt.undofile = true
-vim.opt.updatetime = 250
 vim.opt.autoread = true
-
--- vim.opt.hidden = true
-vim.opt.errorbells = false
-vim.opt.backspace = "indent,eol,start"
--- vim.opt.autochdir = false
-vim.opt.iskeyword:append("-")
-vim.opt.path:append("**")
-vim.opt.mouse = "a"
-vim.opt.clipboard:append("unnamedplus")
+vim.opt.clipboard = "unnamedplus"
+vim.g.netrw_banner = 0
 
 vim.g.mapleader = " "
-vim.g.maplocalleader = " "
 
-vim.keymap.set({ "n", "v" }, "j", "gj", { desc = "Cursor down (line wrap)" })
-vim.keymap.set({ "n", "v" }, "k", "gk", { desc = "Cursor up (line wrap)" })
-vim.keymap.set({ "n", "v" }, "n", "nzzv", { desc = "Next search result (centered)" })
-vim.keymap.set({ "n", "v" }, "N", "<Nzzv", { desc = "Previous search result (centered)" })
-vim.keymap.set({ "n", "v" }, "<C-u>", "<C-u>zz", { desc = "Half page up (centered)" })
-vim.keymap.set({ "n", "v" }, "<C-d>", "<C-d>zz", { desc = "Half page down (centered)" })
+vim.keymap.set({ "n", "v" }, "j", "gj")
+vim.keymap.set({ "n", "v" }, "k", "gk")
+vim.keymap.set({ "n", "v" }, "n", "nzz")
+vim.keymap.set({ "n", "v" }, "N", "<Nzz")
+vim.keymap.set({ "n", "v" }, "<C-u>", "<C-u>zz")
+vim.keymap.set({ "n", "v" }, "<C-d>", "<C-d>zz")
 
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left split" })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right split" })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to bottom split" })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to top split" })
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
 
-vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
-vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
-vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
-vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
-
-vim.keymap.set("v", "<", "<gv", { desc = "Indent left and reselect" })
-vim.keymap.set("v", ">", ">gv", { desc = "Indent right and reselect" })
-
-vim.keymap.set("n", "<leader>e", ":Explore<CR>", { desc = "Open file explorer" })
-vim.keymap.set("n", "<leader>ff", ":find ", { desc = "Find file" })
+vim.keymap.set("n", "<leader>e", ":Explore<CR>")
 
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.keymap.set("n", "<C-c>", "<cmd>nohlsearch<CR>")
 
-vim.keymap.set("n", "<leader>rc", ":e ~/.config/nvim/init.lua<CR>", { desc = "Edit config" })
+vim.keymap.set("n", "<leader>,", ":e ~/.config/nvim/init.lua<CR>")
+vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
 
-local augroup = vim.api.nvim_create_augroup("UserConfig", {})
+vim.keymap.set("n", "gh", vim.diagnostic.open_float)
+vim.keymap.set("n", "gh", vim.lsp.buf.hover)
+
+vim.keymap.set("n", "<leader>r", ":w | !./build.sh<CR>")
+
+vim.pack.add({
+    { src = "https://github.com/neovim/nvim-lspconfig" },
+    { src = "https://github.com/mason-org/mason.nvim" },
+    { src = "https://github.com/zenbones-theme/zenbones.nvim" },
+    { src = "https://github.com/vague2k/vague.nvim" },
+})
+
+require("mason").setup({
+    registries = {
+        "github:mason-org/mason-registry",
+        "github:Crashdummyy/mason-registry",
+    },
+})
+
+vim.lsp.enable({ "lua_ls", "clangd", "roslyn" })
+-- roslyn doesnt seem to get config
+
+vim.lsp.config("lua_ls", {
+    settings = {
+        Lua = {
+            workspace = {
+                library = vim.api.nvim_get_runtime_file("", true)
+            }
+        }
+    }
+})
+
+vim.lsp.config("clangd", { })
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight on yank",
-  group = augroup,
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
-
-vim.api.nvim_create_autocmd("VimResized", {
-  desc = "Auto-resize splits on window resize",
-  group = augroup,
-  callback = function()
-    vim.cmd("tabdo wincmd =")
-  end,
-})
-
-vim.api.nvim_create_autocmd("Colorscheme", {
-  desc = "Set theme background to none",
-  group = augroup,
-  callback = function()
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-    vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+    callback = function()
+        vim.highlight.on_yank()
     end,
 })
-vim.cmd.colorscheme("default")
 
+vim.api.nvim_create_autocmd("ColorScheme", {
+    callback = function()
+        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+        vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+        vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
+
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+        vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+    end,
+})
+vim.g.zenbones_compat = 1
+vim.cmd.colorscheme("zenbones")
