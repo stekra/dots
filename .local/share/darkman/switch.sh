8fcd301ca01021e11066aac0622028f9cee75092
Stefan Kraft <stekra@me.com>
2026-03-13 23:52:50 +0100

    misc

--- .local/share/darkman/switch.sh
+++ .local/share/darkman/switch.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 case "$1" in
     dark)
-        BG_COLOR="222222"
-        BORDER_ACTIVE="ffffff"
-        BORDER_INACTIVE="808080"
+        BG_COLOR="222222ff"
+        BORDER_ACTIVE="ffffffff"
+        BORDER_INACTIVE="808080ff"
         ;;
 
     light)
-        BG_COLOR="e6e6e6"
-        BORDER_ACTIVE="000000"
-        BORDER_INACTIVE="808080"
+        BG_COLOR="e6e6e6ff"
+        BORDER_ACTIVE="000000ff"
+        BORDER_INACTIVE="808080ff"
         ;;
 esac
 
@@ -17,7 +17,7 @@ sed -i "s/background-color.*/background-color \"#${BG_COLOR}\"/" ~/.config/niri/
 sed -i "s/active-color.*/active-color \"#${BORDER_ACTIVE}\"/" ~/.config/niri/overrides.kdl
 sed -i "s/inactive-color.*/inactive-color \"#${BORDER_INACTIVE}\"/" ~/.config/niri/overrides.kdl
 
-hyprctl -i 0 -r keyword misc:background_color       "rgb(${BG_COLOR})"
-hyprctl -i 0 -r keyword general:col.active_border   "rgb(${BORDER_ACTIVE})"
-hyprctl -i 0 -r keyword general:col.inactive_border "rgb(${BORDER_INACTIVE})"
+hyprctl -i 0 -r keyword misc:background_color       "rgba(${BG_COLOR})"
+hyprctl -i 0 -r keyword general:col.active_border   "rgba(${BORDER_ACTIVE})"
+hyprctl -i 0 -r keyword general:col.inactive_border "rgba(${BORDER_INACTIVE})"
 
